5ff7707bc8da973f45b2831c375f1979797514a0
IvanBroz <ivan.brozincevic@student.htw-berlin.de>
2026-05-24 16:31:01 +0200

    Connect frontend to deployed backend

--- src/components/ApplicationList.vue
+++ src/components/ApplicationList.vue
@@ -1,37 +1,21 @@
-<script setup>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
 
-const applications = [
+const applications = ref([])
 
-  {
+const baseUrl = import.meta.env.VITE_API_URL
 
-    company: 'Siemens',
-
-    position: 'Junior Developer',
-
-    status: 'Bewerbung gesendet',
-
-  },
-
-  {
-
-    company: 'BMW',
-
-    position: 'Backend Developer',
-
-    status: 'Interview',
-
-  },
-
-]
-
+onMounted(() => {
+  fetch(`${baseUrl}/applications`)
+    .then(response => response.json())
+    .then(data => {
+      applications.value = data
+    })
+})
 </script>
 
 <template>
-
-  <div v-for="application in applications" :key="application.company">
-
+  <div v-for="application in applications" :key="application.id">
     {{ application }}
-
   </div>
-
 </template>
